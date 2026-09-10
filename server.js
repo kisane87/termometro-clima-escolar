37c6130b28886cd998d2b1d0906767e91204e7e0
Guilherme Dias Oliveira <guilhermeoliveira036789@gmail.com>
2026-09-10 16:53:11 -0300

    Corrige envio de e-mail pelo Gmail

--- server.js
+++ server.js
@@ -18,13 +18,10 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     requireTLS: true,
-    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    logger: true,
-    debug: true
+    }
 });
 
 const port = process.env.PORT || 3000;
